620847309312c7556820da4a7e8907ffc49c6cce
Yash Semlani <yashvsemlani@gmail.com>
2025-07-12 19:46:59 -0500

    initial hnet kernel :

--- fusion/conv_groupnorm_act/naive_conv_groupnorm_act.cu
+++
@@ -1,2 +0,0 @@
-// use thunderkittens to implement a fused conv → groupnorm → activation kernel
-// getting an idea of TK + gearing up for megakernels
---
+++ fusion/hnet/dc.cu
@@ -0,0 +1,118 @@
+/* 
+
+    kernel for the dynamic chunking layer of HNet
+*/
+
+// each warp handles 16 bytes of the input
+// blocks handle 16 * NUM_WARPS bytes of the input
+
+// * denotes element wise multiplication
+
+#include "kittens.cuh"
+using namespace kittens;
+
+// dimensions
+
+constexpr int BATCH_DIM = 1;
+constexpr int SEQ_LEN = 128;
+constexpr int HEAD_DIM = 1024; // d_model = d_k 
+
+// grid dimensions
+#define NUM_WARPS 8
+#define NUM_THREADS (NUM_WARPS * kittens::WARP_THREADS) // 8 * 32 threads per block
+#define NUM_BLOCKS (SEQ_LEN / (NUM_WARPS * 16)) // 128 blocks @ 8 warps per block -> 1024 bytes per block
+
+// inputs are bfloat16
+
+struct dc_globals {
+    using x_gl = gl<bf16, -1, -1, -1, -1, st_bf<16, HEAD_DIM>>; // 16xHEAD tile (loading 16 bytes per warp)
+    using weights_gl = gl<bf16, -1, -1, -1, -1, st_bf<HEAD_DIM, 16>>; // 16x16 tile corresponding to the input spatial location
+    using p_gl = gl<bf16, -1, -1, -1, -1, sv_bf<16>>; // 16 element vector of p values corresponding to the 16 bytes of the block
+    using b_gl = gl<bf16, -1, -1, -1, -1, sv_bf<16>>; // 16 element vector of b values
+
+    x_gl x;
+    weights_gl W_q, W_k;
+    p_gl p;
+    b_gl b;
+}
+
+__global__ __launch_bounds__(NUM_THREADS, 1)
+void tk_dc(const __grid_constant__ dc_globals g) {
+
+    // setup shared memory
+    extern __shared__ alignment_dummy __shm[]; // allocates a dynamic amount of shared memory
+    shared_allocator al((int*)&__shm[0]); // create a shared memory allocator and point it to the starting address of the shared memory
+
+    int byte_id = blockIdx.x * NUM_WARPS + kittens::warpid(); // this is the chunk of 16 bytes that we're processing
+    // block 0 warp 0 processing bytes 0-15
+    // block 0 warp 1 processing bytes 16-31
+    // block 1 warp 0 processing bytes 1024-1039
+
+    // setup shared memory space for Q and K blocks
+
+    // load the x and weights from HBM to shared
+    load(g.x, g.x, {0, 0, byte_id, 0});
+
+    // setup registers
+    rt_bf<16, HEAD_DIM> x_r;
+    rt_bf<HEAD_DIM, 16, kittens::ducks::rt_layout::col> W_q_r;
+    rt_bf<HEAD_DIM, 16, kittens::ducks::rt_layout::col> W_k_r;
+
+    rt_bf<16, 16> Q_r; // 16x16 tile of the Q block
+    rt_bf<16, 16> K_r; // 16x16 tile of the K block
+
+    rv_bf<16> cos_sim; // store the row wise dot products
+    rv_bf<16> q_norm; // store the q row wise norm values
+    rv_bf<16> k_norm; // store the k row wise norm values
+    rv_bf<16> norm; // store the overall norm values
+    rv_bf<16> p; // store the row wise p values
+
+    // load x to registers
+    load(x_r, g.x, {0, 0, byte_id, 0});
+
+    // iterate over the HEAD_DIM dimension in chunks of 16
+    for (int i = 0; i < HEAD_DIM; i += 16) {
+        // load the next column of the weights
+        st_bf<HEAD_DIM, 16> (&W_q_s) = al.allocate<st_bf<HEAD_DIM, 16>>(); // allocate memory for a 16x16 tile of W_q
+        load(W_q_s, g.W_q, {0, 0, byte_id, i});
+        load(W_q_r, W_q_s);
+
+        st_bf<HEAD_DIM, 16> (&W_k_s) = al.allocate<st_bf<HEAD_DIM, 16>>(); // allocate memory for a 16x16 tile of W_k
+        load(W_k_s, g.W_k, {0, 0, byte_id, i});
+        load(W_k_r, W_k_s);
+
+        // multiply x with the weights
+        mma_AB(Q_r, x_r, W_q_r, Q_r); // Q_r is 16x16 tile of the Q block
+        mma_AB(K_r, x_r, W_k_r, K_r); // K_r is 16x16 tile of the K block
+
+        // similarity scores
+        cos_sim = fma_AxBtC::op(Q_r, K_r, cos_sim); // Q_r * K_r + cos_sim 
+
+        // norms
+        q_norm = fma_AxBtC::op(Q_r, Q_r, q_norm); // Q_r * Q_r + q_norm
+        k_norm = fma_AxBtC::op(K_r, K_r, k_norm); // K_r * K_r + k_norm
+
+    }
+
+    // convert to p_t scores
+    // p_t = 1/2 (1 -(Q_t x K_(t-1)^T) / (||Q_t|| * ||K_(t-1)||) )
+    // p = 1/2 (1 - cos_sim / (k_norm * q_norm))
+
+    sqrt(q_norm, q_norm);
+    sqrt(k_norm, k_norm);
+    mul(norm, k_norm, q_norm); // k_norm * q_norm
+    div(p, cos_sim, norm); // cos_sim / (norm)
+    sub(p, 1, p); // 1 - p
+    mul(p, p, 0.5); // 0.5 * (1 - p)
+
+    store(p, g.p, {0, 0, byte_id, 0});
+
+    rv_bf<16> b_r; // accumulator for boundary token values
+    geq(b_r, p, bf16(0.5)); // b_r = p >= 0.5
+    store(b_r, g.b, {0, 0, byte_id, 0}); // store the boundary token values
+
+    // update the x values
+    mul(x_r, x_r, b_r); // x_r = x_r * b_r, broadcasting is handled by TK
+    store(x_r, g.x, {0, 0, byte_id, 0}); // store the updated x values
+
+}
